6cc919183905e10b1335f7d32f70e46f30f5b3f5
Keiroo <orynia55@o2.pl>
2018-08-10 18:39:16 +0200

    Add camera rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
+    private Camera cam;
+    [SerializeField]
     private float movSpeed = 10f;
+    [SerializeField]
+    private float mouseSensitivity = 3f;
 
     private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 camRotation = Vector3.zero;
     private Rigidbody rb;
 
     private void Start()
@@ -21,13 +27,15 @@ public class PlayerController : MonoBehaviour {
     {
         float movX = Input.GetAxisRaw("Horizontal");
         float movY = Input.GetAxisRaw("Vertical");
+        float rotY = Input.GetAxisRaw("Mouse X");
+        float rotX = Input.GetAxisRaw("Mouse Y");
 
         Vector3 vecHorizontal = transform.right * movX;
         Vector3 vecVertical = transform.forward * movY;
 
         velocity = (vecHorizontal + vecVertical).normalized * movSpeed;
-
-
+        rotation = new Vector3(0f, rotY, 0f) * mouseSensitivity;
+        camRotation = new Vector3(rotX, 0f, 0f) * mouseSensitivity;
     }
 
     private void FixedUpdate()
@@ -35,6 +43,11 @@ public class PlayerController : MonoBehaviour {
         if (velocity != Vector3.zero)
         {
             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-camRotation);
         }
     }
 }
