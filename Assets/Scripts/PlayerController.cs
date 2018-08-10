e850f13a28ff80b904de127d51c27e0e26878be9
Keiroo <orynia55@o2.pl>
2018-08-10 18:29:29 +0200

    Add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float movSpeed = 10f;
+
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float movX = Input.GetAxisRaw("Horizontal");
+        float movY = Input.GetAxisRaw("Vertical");
+
+        Vector3 vecHorizontal = transform.right * movX;
+        Vector3 vecVertical = transform.forward * movY;
+
+        velocity = (vecHorizontal + vecVertical).normalized * movSpeed;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+}
