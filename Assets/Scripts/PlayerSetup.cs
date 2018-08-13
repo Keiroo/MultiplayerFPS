ef4a16ea10b681cdedf0e445720c25475809c4c0
Keiroo <orynia55@o2.pl>
2018-08-13 15:54:28 +0200

    Network basic syncing

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] Components;
+    GameObject MainCamera;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (Behaviour component in Components)
+            {
+                component.enabled = false;
+            }
+        }
+        else
+        {
+            MainCamera = Camera.main.gameObject;
+            if (MainCamera != null)
+            {
+                MainCamera.SetActive(false);
+            }            
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (MainCamera != null)
+        {
+            MainCamera.SetActive(true);
+        }
+    }
+
+}
