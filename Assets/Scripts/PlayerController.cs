c7846a9787ccd388697e903d29b29e695790a083
Keiroo <orynia55@o2.pl>
2018-08-31 15:03:13 +0200

    Add in-air movement, modify physics values

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,13 +13,19 @@ public class PlayerController : MonoBehaviour {
     private float jumpHeight = 1f;
     [SerializeField]
     private float jumpForce = 1f;
+    [SerializeField]
+    private float airVelocityForce = 1f;
+    [SerializeField]
+    private float upAirVelocityForce = 1f;
 
     private Vector3 velocity = Vector3.zero;
     private Vector3 rotation = Vector3.zero;
     private Vector3 camRotation = Vector3.zero;
     private Vector3 jumpVector = Vector3.zero;
+    private Vector3 airVelocity = Vector3.zero;
     private Rigidbody rb;
     private bool isGrounded;
+    private bool jumpedWithoutVelocity;
 
     private void Start()
     {
@@ -28,6 +34,10 @@ public class PlayerController : MonoBehaviour {
 
     private void Update()
     {
+        // Reset model rotation
+        gameObject.transform.rotation = Quaternion.Euler(
+            new Vector3(0, gameObject.transform.rotation.eulerAngles.y, 0));
+
         float movX = Input.GetAxisRaw("Horizontal");
         float movY = Input.GetAxisRaw("Vertical");
         float rotY = Input.GetAxisRaw("Mouse X");
@@ -42,6 +52,10 @@ public class PlayerController : MonoBehaviour {
         {
             velocity = (vecHorizontal + vecVertical).normalized * movSpeed;
         }
+        else
+        {
+            velocity = Vector3.zero;
+        }
         
         rotation = new Vector3(0f, rotY, 0f) * mouseSensitivity;
         camRotation = new Vector3(rotX, 0f, 0f) * mouseSensitivity;
@@ -51,23 +65,33 @@ public class PlayerController : MonoBehaviour {
         {
             if (velocity == Vector3.zero)
             {
-                jumpVector = Vector3.up;
+                jumpedWithoutVelocity = true;
+                jumpVector = Vector3.up / 2f;
             }
             else
             {
-                jumpVector = ((Vector3.up * jumpHeight) + velocity.normalized).normalized;
+                jumpedWithoutVelocity = false;
+                jumpVector = ((Vector3.up * jumpHeight) + velocity).normalized;
             }
             isGrounded = false;
         }
         else jumpVector = Vector3.zero;
+
+        // If in air, weaken the move force
+        if (!isGrounded)
+            airVelocity = (vecHorizontal + vecVertical).normalized * (movSpeed / 5.0f);
     }
 
     private void FixedUpdate()
     {
         // Apply player movement, rotation and camera rotation
-        if (velocity != Vector3.zero)
+        if (velocity != Vector3.zero && isGrounded)
         {
             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+        else if (velocity != Vector3.zero)
+        {
+            rb.AddForce(velocity / 0.5f, ForceMode.Force);
         }
         rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
 
@@ -80,6 +104,16 @@ public class PlayerController : MonoBehaviour {
         if (jumpVector != Vector3.zero)
         {
             rb.AddForce(jumpVector * jumpForce, ForceMode.Impulse);            
+        }
+
+        // When in air, use AddForce instead of MovePosition
+        if (airVelocity != Vector3.zero && !isGrounded)
+        {
+            if (jumpedWithoutVelocity)
+            {
+                rb.AddForce(airVelocity * upAirVelocityForce, ForceMode.Force);
+            }
+            else rb.AddForce(airVelocity * airVelocityForce, ForceMode.Force);
         }
     }
 
