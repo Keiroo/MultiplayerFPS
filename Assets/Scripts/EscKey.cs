bdcb514f37b8683da18c56dcf7040deb3bed9172
Keiroo <orynia55@o2.pl>
2018-09-17 17:43:05 +0200

    Add escape key functionality

---
+++ Assets/Scripts/EscKey.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class EscKey : MonoBehaviour {
+
+    [SerializeField]
+    private string sceneName;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (sceneName == "Exit" || sceneName == "Quit") Application.Quit();
+            else SceneManager.LoadScene(sceneName);
+        }
+    }
+}
